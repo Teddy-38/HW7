3534eeea070d42c096f294ff0573f671d985c0d1
Teddy-38 <Zhdanov_as94@mail.ru>
2025-01-25 11:51:09 +0800

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Task1");
+        double deposit = 15000;
+        double total = 0;
+        int month = 0;
+        while (total < 2459000) {
+            total += deposit;;
+            month += 1;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+        }
     }
 }
