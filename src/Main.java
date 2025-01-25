6a379d5be4d337654946be762b4920ce4819d062
Teddy-38 <Zhdanov_as94@mail.ru>
2025-01-25 12:05:25 +0800

    Task2

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,22 @@ public class Main {
         double total = 0;
         int month = 0;
         while (total < 2459000) {
-            total += deposit;;
+            total += deposit;
+            ;
             month += 1;
             System.out.println("Месяц " + month + ", сумма накоплений равна " + total + " рублей");
+        }
+        System.out.println("Task2");
+        int start = 1;
+        while (start <= 10) {
+            System.out.print(start + " ");
+            start = start + 1;
+        }
+        System.out.println();
+        int start2 = 10;
+        while (start2 > 0) {
+            System.out.print(start2 + " ");
+            start2 = start2 - 1;
         }
     }
 }
